833ccdd124cb9e74cbc374b96da3c0b2bfbc6c5d
Tung Anh Nguyen <49914343+nhimzu123@users.noreply.github.com>
2021-05-25 10:45:23 +0700

    Update run_translate.sh

--- run_translate.sh
+++ run_translate.sh
@@ -8,4 +8,4 @@ export CUDA_VISIBLE_DEVICES=3 && python translate_100000.py data/train_ner_90k_1
 export CUDA_VISIBLE_DEVICES=4 && python translate_100000.py data/train_ner_120k_150k.vi translation/train_ner_120k_150k.zh &
 export CUDA_VISIBLE_DEVICES=5 && python translate_100000.py data/train_ner_150k_180k.vi translation/train_ner_150k_180k.zh &
 export CUDA_VISIBLE_DEVICES=6 && python translate_100000.py data/train_ner_180k_210k.vi translation/train_ner_180k_210k.zh &
-export CUDA_VISIBLE_DEVICES=6 && python translate_100000.py data/train_ner_210k_220k.vi translation/train_ner_210k_220k.zh &
+export CUDA_VISIBLE_DEVICES=7 && python translate_100000.py data/train_ner_210k_220k.vi translation/train_ner_210k_220k.zh &
