fdce3072c0a32cca3b108648df2baf9b57c648dc
Tung Anh Nguyen <49914343+nhimzu123@users.noreply.github.com>
2021-05-27 09:51:17 +0700

    Update run_translate.sh

--- run_translate.sh
+++ run_translate.sh
@@ -2,7 +2,7 @@
 # Translate 800,000 sentences using 8 GPUs.
 
 export CUDA_VISIBLE_DEVICES=0 && python translate_100000.py data/train_ner_0_30k.vi translation/train_ner_0_30k.zh &
-export CUDA_VISIBLE_DEVICES=1 && python translate_100000.py data/train_ner_30_60k.vi translation/train_ner_30_60k.zh &
+export CUDA_VISIBLE_DEVICES=1 && python translate_100000.py data/train_ner_30k_60k.vi translation/train_ner_30k_60k.zh &
 export CUDA_VISIBLE_DEVICES=2 && python translate_100000.py data/train_ner_60k_90k.vi translation/train_ner_60k_90k.zh &
 export CUDA_VISIBLE_DEVICES=3 && python translate_100000.py data/train_ner_90k_120k.vi translation/train_ner_90k_120k.zh &
 export CUDA_VISIBLE_DEVICES=4 && python translate_100000.py data/train_ner_120k_150k.vi translation/train_ner_120k_150k.zh &
